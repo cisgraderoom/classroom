f3bef017064d1771d1eb9780d59e1eef1bac4878
Kittidet Cheavachirakul <fvgbhv14@gmail.com>
2022-01-08 16:07:55 +0700

    feat: Infinite Scroll

--- src/components/ListPost.vue
+++ src/components/ListPost.vue
@@ -19,7 +19,11 @@
             >ไม่มีโพสในชั้นเรียน</v-alert
         >
 
-        <v-row v-for="(post, index) in textPost" :key="index">
+        <v-row
+            v-for="(post, index) in textPost"
+            :key="index"
+            ref="infinite-list"
+        >
             <v-col md="9" class="mx-auto" xl="5">
                 <v-lazy transition="fade-transition">
                     <Post
@@ -34,6 +38,14 @@
                 </v-lazy>
             </v-col>
         </v-row>
+        <v-col md="9" class="mx-auto" xl="5">
+            <div
+                ref="infinitescrolltrigger"
+                id="scoll-trigger"
+                :disabled="this.$store.state.nextPost.isLoading"
+            ></div>
+            <SkeletonPost v-show="this.$store.state.nextPost.isLoading" />
+        </v-col>
     </div>
 </template>
 
@@ -50,12 +62,16 @@ export default {
     },
     mounted() {
         this.getListPost()
+        this.getNextPost()
     },
+
     data: () => ({
         textPost: [],
         isActive: false,
         upDateKey: 0,
         checkAddClassroom: JSON.parse(localStorage.getItem('user')).role,
+        currentPage: 1,
+        hasNext: false,
     }),
     methods: {
         getListPost() {
@@ -64,9 +80,38 @@ export default {
                 .dispatch('listPost/listPost', { classcode })
                 .then(() => {
                     this.textPost = this.$store.state.listPost.list
+                    this.hasNext = this.$store.state.listPost.hasNext
                     this.upDateKey += 1
                 })
             return data
+        },
+        getNextPost() {
+            if (!this.$store.state.nextPost.isLoading) {
+                const observer = new IntersectionObserver((entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.intersectionRatio > 0 && this.hasNext) {
+                            const classcode = this.$route.params.code
+                            this.currentPage += 1
+                            console.log('getnext ', this.currentPage)
+                            let data = this.$store
+                                .dispatch('nextPost/nextPost', {
+                                    classcode,
+                                    currentPage: this.currentPage,
+                                })
+                                .then(() => {
+                                    this.textPost = [
+                                        ...this.textPost,
+                                        ...this.$store.state.nextPost.list,
+                                    ]
+                                    this.hasNext =
+                                        this.$store.state.nextPost.hasNext
+                                })
+                            return data
+                        }
+                    })
+                })
+                observer.observe(this.$refs.infinitescrolltrigger)
+            }
         },
     },
 }
