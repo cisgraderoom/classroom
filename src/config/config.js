b54c6a3a8c0f39efa3a96671e8a7df1c96e2e519
Kittidet Cheavachirakul <fvgbhv14@gmail.com>
2022-03-28 22:08:51 +0700

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 export const setting = {
-    // backendURL: 'https://backend-cis.t6tg.com/api/',
-    backendURL: 'https://api-cis.t6tg.com/api/',
+    backendURL: 'https://backend-cis.t6tg.com/api/',
 }
 export default setting
